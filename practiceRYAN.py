52182a9ba0b79834f9e17b754da942eaf183eecd
Aashni Jain <aashnijain@Aashnis-MacBook-Pro.local>
2020-02-25 11:39:55 -0800

    i hate ryan i messed up his edit

--- practiceRYAN.py
+++ practiceRYAN.py
@@ -1,7 +1,7 @@
 # partner 1 Ryan Westcott
 # partner 2 Aashni
 
-# hello world
+# why am i here
 
 def getNRandom(n):
 	'''takes in an integer and returns a list of n random integers between 1 and 10, inclusive'''
