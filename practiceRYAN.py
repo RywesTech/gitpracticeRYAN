c9486da678822270b356fa83c7c6c70b16a0b5f8
Aashni Jain <aashni.jain@gmail.com>
2020-02-26 11:19:34 -0800

    second function

--- practiceRYAN.py
+++ practiceRYAN.py
@@ -4,15 +4,19 @@
 # hello world Ryan
 
 def getNRandom(n):
-	'''takes in an integer and returns a list of n random integers between 1 and 10, inclusive'''
+    '''takes in an integer and returns a list of n random integers between 1 and 10, inclusive'''
     pass
 
 def multiplyRandom(numbers):
-	'''takes in a list of n numbers and returns the product of the numbers'''
-    pass
+    '''takes in a list of n numbers and returns the product of the numbers'''
+    product=1
+    for entry in numbers:
+        product=product*entry
+    return product
 
 def main():
-	print(multiplyRandom(getNRandom(10))
+    print(multiplyRandom([1,5,3,2,8]))
+#   print(multiplyRandom(getNRandom(10)))
 
 if __name__ == "__main__":
-	main()
+    main()
